1682d3204590491cb290d84e7b42d7ee34ecea23
Varvara Gudanova <gudanova-v@mail.ru>
2019-02-20 23:03:29 +0300

    added reverselist

--- 4 semestr/Homework1/Homework1/reverseList.fsx
+++ 4 semestr/Homework1/Homework1/reverseList.fsx
@@ -1,1 +1,10 @@
-﻿
+﻿let reverse (list:list<_>) =
+    let rec reverseHelp (list:list<_>) (list2:list<_>) currentEl =
+        if currentEl = -1 then list2
+        else
+            reverseHelp list (list2 @ [list.Item(currentEl)]) (currentEl - 1)
+    match list.Length with
+    | 1 -> list
+    | _ -> reverseHelp list [] (list.Length - 1)
+
+reverse ['a';'b']
