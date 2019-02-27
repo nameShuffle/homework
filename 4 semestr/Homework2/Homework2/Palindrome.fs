fdfa3928de754812d7d2d91d06db8b967ef1cc72
Varvara Gudanova <gudanova-v@mail.ru>
2019-02-27 22:29:08 +0300

    added palindrome and findelement

---
+++ 4 semestr/Homework2/Homework2/Palindrome.fs
@@ -0,0 +1,14 @@
+﻿module Palindrome
+
+let isPalindrome2 (string:string) =
+    let reverse (list:list<_>) =
+        let rec reverseHelp (list1:list<_>) (list2:list<_>) numberOfElement =
+            if numberOfElement = list.Length then list2
+            else
+                reverseHelp list1.Tail (list1.Head :: list2) (numberOfElement + 1)
+
+        if list.Length = 1 || list.Length = 0 then list
+        else reverseHelp list [] 0
+        
+    if reverse (Seq.toList string) = (Seq.toList string) then true
+    else false
