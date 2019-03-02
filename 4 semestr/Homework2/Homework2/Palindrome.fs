ffcc66edc1afc2d07161bee3f76a40f4055c820f
Varvara Gudanova <gudanova-v@mail.ru>
2019-03-02 23:09:03 +0300

    added mergesort

--- 4 semestr/Homework2/Homework2/Palindrome.fs
+++ 4 semestr/Homework2/Homework2/Palindrome.fs
@@ -9,6 +9,6 @@ let isPalindrome (string:string) =
 
         if list.Length = 1 || list.Length = 0 then list
         else reverseHelp list [] 0
-        
+
     if reverse (Seq.toList string) = (Seq.toList string) then true
     else false
