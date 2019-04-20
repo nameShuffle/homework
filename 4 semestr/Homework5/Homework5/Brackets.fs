3e96c57e40203e1e384c6c95ffadbd476b215182
Varvara Gudanova <gudanova-v@mail.ru>
2019-04-20 22:17:33 +0300

    first two tasks

---
+++ 4 semestr/Homework5/Homework5/Brackets.fs
@@ -0,0 +1,31 @@
+﻿module Brackets
+
+///Проверка корректности расстановки скобок в строке. Всего может быть три вида скобок:
+/// [], {}, ()
+/// Корретность расстановки другого вида скобок не проверяется.
+let brackets string = 
+    let stringInList = Seq.toList string
+
+    let rec handleString currentString listOfOpenBrackets =  
+        match currentString with
+        | head :: tail -> 
+            if head = '(' || head = '[' || head = '{' then
+                handleString tail (head :: listOfOpenBrackets)
+            else if head = ')' || head = ']' || head = '}' then
+                match listOfOpenBrackets with
+                | headBrackets :: tailBrackets ->
+                    if headBrackets = '(' && head = ')' then
+                        handleString tail tailBrackets
+                    else if headBrackets = '{' && head = '}' then
+                        handleString tail tailBrackets
+                    else if headBrackets = '[' && head = ']' then 
+                        handleString tail tailBrackets
+                    else "последовательность скобок не корректна"
+                | _ -> "последовательность скобок не корректна"
+            else handleString tail listOfOpenBrackets
+        | _ -> 
+            match listOfOpenBrackets with 
+            | head :: tail -> "последовательность скобок не корректна"
+            | _ -> "последовательность скобок корректна"
+
+    handleString stringInList []
---
+++ 4 semestr/Homework5/Homework5/PhoneBook.fs
@@ -0,0 +1,48 @@
+﻿module PhoneBook
+
+open System
+open System.IO
+
+type person = 
+   { name : string
+     phone : string }
+
+let phoneBook = 
+    let rec phoneBookAction book = 
+        printf "New command: "
+        let command = Console.ReadLine()
+        if command = "add" then
+            printf "Name: "
+            let name = Console.ReadLine()
+            printf "Phone: "
+            let phone = Console.ReadLine()
+            let newPerson = { name = name; phone = phone}
+
+            phoneBookAction (newPerson :: book)
+        else if command = "searchByName" then
+            printf "Name: "
+            let nameToSearch = Console.ReadLine()
+            let info = List.find (fun y -> y.name = nameToSearch) book
+            
+            printfn "Founded phone: %s" info.phone
+            phoneBookAction book
+        else if command = "searchByPhone" then
+            printf "Phone: "
+            let phoneToSearch = Console.ReadLine()
+            let info = List.find (fun y -> y.phone = phoneToSearch) book
+
+            printfn "Founded name: %s" info.name
+            phoneBookAction book
+        else if command ="saveToFile" then
+            printf "File name: "
+            let fileName = Console.ReadLine()
+            use file = File.CreateText(fileName) 
+            List.iter (fun elem -> file.WriteLine(elem.name); file.WriteLine(elem.phone)) book
+            
+            phoneBookAction book
+        else if command = "exit" then 0 
+        else 
+            printfn "incorrect command"
+            phoneBookAction book
+
+    phoneBookAction []
