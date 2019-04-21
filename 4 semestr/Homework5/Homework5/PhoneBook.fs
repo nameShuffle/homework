ab0794f9fbffd5232e60d1e66f36cfe7166c2bfe
Varvara Gudanova <gudanova-v@mail.ru>
2019-04-21 21:11:12 +0300

    first version of the last task

--- 4 semestr/Homework5/Homework5/PhoneBook.fs
+++ 4 semestr/Homework5/Homework5/PhoneBook.fs
@@ -3,9 +3,19 @@
 open System
 open System.IO
 
-type person = 
-   { name : string
-     phone : string }
+type Person = 
+   { Name : string
+     Phone : string }
+
+let rec readPersonsInfoFromFile (reader : StreamReader) currentList = 
+    match reader.ReadLine() with
+    | null -> reader.Close(); currentList 
+    | name -> 
+        let phone = reader.ReadLine()
+        let newPerson = { Name = name; Phone = phone}
+        readPersonsInfoFromFile reader (newPerson :: currentList)
+
+let findPersonByName name = 
 
 let phoneBook = 
     let rec phoneBookAction book = 
@@ -16,33 +26,50 @@ let phoneBook =
             let name = Console.ReadLine()
             printf "Phone: "
             let phone = Console.ReadLine()
-            let newPerson = { name = name; phone = phone}
+            let newPerson = { Name = name; Phone = phone}
 
             phoneBookAction (newPerson :: book)
-        else if command = "searchByName" then
+        else if command = "search by name" then
             printf "Name: "
             let nameToSearch = Console.ReadLine()
-            let info = List.find (fun y -> y.name = nameToSearch) book
+            let info = List.find (fun person -> person.Name = nameToSearch) book
             
-            printfn "Founded phone: %s" info.phone
+            printfn "Founded phone: %s" info.Phone
             phoneBookAction book
-        else if command = "searchByPhone" then
+        else if command = "search by phone" then
             printf "Phone: "
             let phoneToSearch = Console.ReadLine()
-            let info = List.find (fun y -> y.phone = phoneToSearch) book
+            let info = List.find (fun person -> person.Phone = phoneToSearch) book
 
-            printfn "Founded name: %s" info.name
+            printfn "Founded name: %s" info.Name
             phoneBookAction book
-        else if command ="saveToFile" then
-            printf "File name: "
-            let fileName = Console.ReadLine()
-            use file = File.CreateText(fileName) 
-            List.iter (fun elem -> file.WriteLine(elem.name); file.WriteLine(elem.phone)) book
+        else if command = "show all" then
+            List.iter (fun person -> 
+                printf "%s " person.Name 
+                printfn "%s" person.Phone) book
             
             phoneBookAction book
+        else if command ="save to file" then
+            printf "File name: "
+            let fileName = Console.ReadLine()
+            let writer = File.CreateText fileName 
+            List.iter (fun person -> 
+                fprintfn writer "%s" person.Name 
+                fprintfn writer "%s" person.Phone) book
+                
+            writer.Close()
+            phoneBookAction book
+        else if command = "get from file" then
+            printf "File name: "
+            let fileName = Console.ReadLine()
+            let reader = File.OpenText fileName
+            let newBook = readPersonsInfoFromFile reader book
+
+            phoneBookAction newBook
         else if command = "exit" then 0 
         else 
             printfn "incorrect command"
             phoneBookAction book
 
     phoneBookAction []
+
