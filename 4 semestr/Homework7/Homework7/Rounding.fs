314f64ff80702e4138a5fe4cbcfe0990f4050b60
Varvara Gudanova <gudanova-v@mail.ru>
2019-05-08 21:40:00 +0300

    tasks and test

---
+++ 4 semestr/Homework7/Homework7/Rounding.fs
@@ -0,0 +1,9 @@
+﻿module Rounding
+
+open System
+
+type RoundingBuilder(number : int) =
+    member this.Bind(x : float, func) =
+        x |> func
+    member this.Return(x : float) =
+        Math.Round(x, number)
--- 4 semestr/Test2/Test2/Sequences.fs
+++ 4 semestr/Test2/Test2/Sequences.fs
@@ -5,3 +5,7 @@ let infiniteNumbers = Seq.initInfinite (fun n -> n + 1)
 
 let createSeq () = 
     Seq.map2 (fun x y -> x * y) infiniteSigns infiniteNumbers
+
+printfn "%A" infiniteSigns
+printfn "%A" infiniteNumbers
+printfn "%A" (createSeq ())
