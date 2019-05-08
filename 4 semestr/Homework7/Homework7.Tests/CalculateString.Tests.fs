70a13175d73143ada860205b206870c1e0794593
Varvara Gudanova <gudanova-v@mail.ru>
2019-05-08 22:06:12 +0300

    tasks and tests

---
+++ 4 semestr/Homework7/Homework7.Tests/CalculateString.Tests.fs
@@ -0,0 +1,39 @@
+﻿namespace Tests
+
+open NUnit.Framework
+open FsUnit
+
+module ``Tests for CalculateString task`` =
+    open CalculateString
+
+    let calculate = new CalculateStringBuilder()
+
+    [<Test>]
+    let ``normal string calculation test with adding`` () =
+        let result = calculate {
+            let! x = "25"
+            let! y = "12"
+            let z = x + y
+            return z
+        }
+        result |> should equal (Some 37)
+
+    [<Test>]
+    let ``string calculation test with incorrect strings`` () =
+        let result = calculate {
+            let! x = "25"
+            let! y = "hello"
+            let z = x + y
+            return z
+        }
+        result |> should equal None
+
+    [<Test>]
+    let ``normal string calculation test with multiplying`` () =
+        let result = calculate {
+            let! x = "25"
+            let! y = "4"
+            let z = x * y
+            return z
+        }
+        result |> should equal (Some 100)
---
+++ 4 semestr/Homework7/Homework7/CalculateString.fs
@@ -0,0 +1,9 @@
+﻿module CalculateString
+
+type CalculateStringBuilder() =
+    member this.Bind(str : string, func) =
+        match System.Int32.TryParse(str) with
+        | (true, number) -> number |> func
+        | _ -> None
+    member this.Return(x) =
+        Some x
