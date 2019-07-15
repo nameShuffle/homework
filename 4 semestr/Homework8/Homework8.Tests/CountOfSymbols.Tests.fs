f198d150196206a903256d4396534aa398eafd9b
Varvara Gudanova <gudanova-v@mail.ru>
2019-07-15 16:55:52 +0300

    добавлено второе задание

---
+++ 4 semestr/Homework8/Homework8.Tests/CountOfSymbols.Tests.fs
@@ -0,0 +1,11 @@
+namespace Tests
+
+open NUnit.Framework
+open FsUnit
+
+module ``Tests for CalculateString task`` =
+    open CountOfSymbols
+
+    //[<Test>]
+    //let ``normal string calculation test with adding`` () =
+     //   let result = checkAllUrls 
---
+++ 4 semestr/Homework8/Homework8/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System.Net
+open System.IO
+open System.Text.RegularExpressions
+
+[<EntryPoint>]
+let main argv =
+    let pattern = "<a\shref=\"(https?://.*?)\">"
+    let regex = new Regex(pattern)
+    let text = File.ReadAllText("hello.txt")
+    let get = regex.Matches(text)
+    0 // return an integer exit code
